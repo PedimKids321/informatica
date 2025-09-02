250629c66721b6375b5c0057f5fb135e16c112ec
PedimKids321 <pedrohenriquesilvafilsner@gmail.com>
2025-09-02 10:28:57 -0300

    Add files via upload

---
+++ EX7_MIRANHA E THANOS.py
@@ -0,0 +1,8 @@
+Thanos=1.50
+Miranha=1.10
+for i in range (1000):
+    if Miranha>Thanos:
+        break
+    Thanos+=0.02
+    Miranha+=0.03
+print("Vai demorar", i, "anos para que o Homem Aranha seja maior que o Thanos!")
---
+++ EX8.py
@@ -0,0 +1,5 @@
+numero= int(input("Digite um número: "))
+fatorial= 1
+for i in range(1, numero+1):
+    fatorial*= i
+print("O fatorial do número ", numero, "é", fatorial)
